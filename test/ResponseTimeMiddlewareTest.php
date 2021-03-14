a3297b5144c0c13371838f31456d591d58d6e38d
Jonathan Maron <jonathan.maron@theimagingsource.com>
2021-03-14 05:14:04 +0100

    Added tests

---
+++ test/ResponseTimeMiddlewareTest.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+
+namespace CtwTest\Middleware\ResponseTimeMiddleware;
+
+use Ctw\Middleware\ResponseTimeMiddleware\ResponseTimeMiddleware;
+use Middlewares\Utils\Dispatcher;
+use Middlewares\Utils\Factory;
+
+class ResponseTimeMiddlewareTest extends AbstractCase
+{
+    public function testResponseTimeMiddleware()
+    {
+        $stack    = [
+            new ResponseTimeMiddleware(),
+        ];
+        $response = Dispatcher::run($stack);
+
+        $string = $response->getHeaderLine('X-Response-Time');
+
+        self::assertMatchesRegularExpression('/^\d{1,4}\.\d{3} ms$/', $string);
+    }
+
+    public function testResponseTimeMiddlewareAsFloat()
+    {
+        $serverParams = [
+            'REQUEST_TIME_FLOAT' => microtime(true),
+        ];
+        $request      = Factory::createServerRequest('GET', '/', $serverParams);
+        $stack        = [
+            new ResponseTimeMiddleware(),
+        ];
+        $response     = Dispatcher::run($stack, $request);
+
+        $string = $response->getHeaderLine('X-Response-Time');
+
+        self::assertMatchesRegularExpression('/^\d{1,4}\.\d{3} ms$/', $string);
+    }
+}
