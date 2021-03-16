0651093b8393f9bdb4df96d0f40337abdaf0f679
Jonathan Maron <jonathan.maron@theimagingsource.com>
2021-03-16 10:18:34 +0100

    Improved tests

---
+++ test/ConfigProviderTest.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace CtwTest\Middleware\ResponseTimeMiddleware;
+
+use Ctw\Middleware\ResponseTimeMiddleware\ConfigProvider;
+use Ctw\Middleware\ResponseTimeMiddleware\ResponseTimeMiddleware;
+use Ctw\Middleware\ResponseTimeMiddleware\ResponseTimeMiddlewareFactory;
+
+class ConfigProviderTest extends AbstractCase
+{
+    public function testConfigProvider(): void
+    {
+        $configProvider = new ConfigProvider();
+
+        $expected = [
+            'dependencies' => [
+                'factories' => [
+                    ResponseTimeMiddleware::class => ResponseTimeMiddlewareFactory::class,
+                ],
+            ],
+        ];
+
+        $this->assertSame($expected, $configProvider->__invoke());
+    }
+}
